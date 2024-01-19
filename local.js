c33879794a6fc7eaf3766ffc85b01077145b4eb0
sk-nagur-baji <109502192+sk-nagur-baji@users.noreply.github.com>
2024-01-19 16:20:53 +0530

    Add files via upload

---
+++ local.js
@@ -0,0 +1,6 @@
+var calc = require('./local');
+var x= 50 , y = 10;
+console.log("Addition "+ local.add(x, y));
+console.log("Subtraction "+ local.subtract(x, y));
+console.log("Multiply "+ local.multiply(x, y));
+console.log("Divide "+ local.divide(x, y));
